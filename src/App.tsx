7cb95c7a4f302c2b4bea4420690e17c0a2d5ce53
Caleb TUYISINGIZE <calebmevis11@gmail.com>
2026-02-16 15:50:51 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,26 @@ function App() {
       <div className="flex flex-wrap gap-3">
 
         <Contacts
-          img='../../public/cats/Cat Wallpaper.jpg'
+          img='/Cat Wallpaper.jpg'
           name='Mr. Whiskerson'
           phone='(212) 555-1234'
           email='mr.whiskaz@catnap.meow'
 
         />
         <Contacts
-          img='../../public/cats/download (1).jpg'
+          img='/download (1).jpg'
           name='Fluffykins'
           phone='(212) 555-2345'
           email='fluff@me.com'
         />
         <Contacts
-          img='../../public/cats/download.jpg'
+          img='/download.jpg'
           name='Felix'
           phone='(212) 555-4567'
           email='thecat@hotmail.com'
         />
         <Contacts
-          img='../../public/cats/НейроОткрытки для Любимых❤️.jpg'
+          img='/НейроОткрытки для Любимых❤️.jpg'
           name='Pumpkin'
           phone='(0800) CAT KING'
           email='pumpkin@scrimba.com'
